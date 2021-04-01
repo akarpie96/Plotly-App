e340dcef8a49780a94ceb0132381019901dd32c6
Aaron Karpie <aaronkarpie@gmail.com>
2021-03-31 22:30:29 -0700

    still working on getting init x and y values

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,30 +1,32 @@
-d3.json("samples.json").then((data)=> {
-    console.log(data)
-var names=data.names;
-console.log(names)})
+var dropdown = d3.select("#selDataset");
 
 
+function Init(){
+    d3.json("samples.json").then((data)=> {
+            console.log(data)
 
-// function Init(){
-//  var dropdown = d3.select("#selDataset");
-//     d3.json("samples.json").then((data)=> {
-//             console.log(data)
-
-//     var names = data.names
+    var names = data.names
 
  
-//     names.forEach(function(name) {
-//         dropdown.append("option").text(name).property("value")});   
-//     })}
+    names.forEach(function(name) {
+        dropdown.append("option").text(name).property("value")})
+    var samplesInit= data.samples[0].sample_values
+    var otuInit = data.samples[0].otu_labels
+    console.log(samplesInit)
+    console.log(otuInit)
+    })}
 
-// // handling change
-// // d3.selectAll("#selDataset").on("change", getData )
+// handling change
+// d3.selectAll("#selDataset").on("change", getData )
 // function getData(){
 // var dropdownMenu = d3.select("#selDataset");
 // var dataset = dropdownMenu.property("value");
+
+// if (dataset === 'option')
+// x=[]
 
 // }
 
 
     
-// Init()
+Init()
