71a30ac2e196ae8847dd10619daa70c911659759
Aaron Karpie <aaronkarpie@gmail.com>
2021-03-31 21:40:21 -0700

    adding to initial chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,17 +1,30 @@
+d3.json("samples.json").then((data)=> {
+    console.log(data)
+var names=data.names;
+console.log(names)})
 
-// 
-// d3.selectAll("#selDataset").on("change", getData )
 
-function getplotdata(){
-    var dropdown = d3.select("#selDataset");
-    d3.json("samples.json").then((data)=> {
-            console.log(data)
 
-    var names = data.names
-    console.log(names)
+// function Init(){
+//  var dropdown = d3.select("#selDataset");
+//     d3.json("samples.json").then((data)=> {
+//             console.log(data)
+
+//     var names = data.names
+
  
-    names.forEach(function(name) {
-        dropdown.append("option").text(name).property("value")});   
-    })}
+//     names.forEach(function(name) {
+//         dropdown.append("option").text(name).property("value")});   
+//     })}
+
+// // handling change
+// // d3.selectAll("#selDataset").on("change", getData )
+// function getData(){
+// var dropdownMenu = d3.select("#selDataset");
+// var dataset = dropdownMenu.property("value");
+
+// }
+
+
     
-    getplotdata()
+// Init()
