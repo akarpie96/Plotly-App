0eb4cbfc9498d58757597ac81fb75cdce63d1441
Aaron Karpie <aaronkarpie@gmail.com>
2021-03-31 17:56:41 -0700

    got dropdown menu to display items

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,17 @@
+
+// 
+// d3.selectAll("#selDataset").on("change", getData )
+
 function getplotdata(){
+    var dropdown = d3.select("#selDataset");
     d3.json("samples.json").then((data)=> {
             console.log(data)
+
+    var names = data.names
+    console.log(names)
+ 
+    names.forEach(function(name) {
+        dropdown.append("option").text(name).property("value")});   
     })}
     
     getplotdata()
