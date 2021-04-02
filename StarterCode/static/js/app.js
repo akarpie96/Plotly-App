29b2efb927321c2bcac978b2e51304f574f9dda8
Aaron Karpie <aaronkarpie@gmail.com>
2021-04-02 13:00:35 -0700

    adding list items for demographic data

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -21,8 +21,8 @@ var dropdown = d3.select("#selDataset");
 //     })}
 //     Init()
 // handling change
-// d3.selectAll("#selDataset").on("change", getData )
-
+d3.selectAll("#selDataset").on("change", getData )
+function getData(){
 //Reading in samples.json file
 d3.json("samples.json").then((data)=> {
      console.log(data)
@@ -33,14 +33,18 @@ console.log(names)
 names.forEach(function(name) {
      
 dropdown.append("option").text(name).property("value")})
-//Selection Variable
+//Selection Variable and Creating Dropdown
 var dropdownMenu = d3.select("#selDataset");
 var selection = dropdownMenu.property("value");
 
-//Getting otu IDS
-var filteredData= data.samples.filter(id => id.id === selection)
+//Creating list
+var list = d3.select("#sample-metadata").append("ul")
 
+//Getting otu IDS and Demographic 
+var filteredData= data.samples.filter(id => id.id === selection)
+var filteredDemo=data.metadata.filter(meta=>meta.id.toString() === selection)
 console.log(filteredData)
+console.log(filteredDemo)
 
 var filteredID = filteredData.map(id=>id.otu_ids)
 console.log(filteredID)
@@ -54,11 +58,35 @@ console.log(filteredSample)
 var slicedID = filteredID[0].slice(0,10)
 
 var slicedSample = filteredSample[0].slice(0,10)
-
 console.log(slicedID)
 console.log(slicedSample)
 
+var trace1={
+    x: slicedSample,
+    y: slicedID, 
+    text: slicedID, 
+    type: "bar",
+    orientation: "h"
+};
 
-})
 
-// getData()
+var data = [trace1];
+
+var layout = {
+    title: "Belly Button", 
+    margin: {
+        l:100,
+        r:100,
+        t:100,
+        b:100
+    }
+}
+
+Plotly.newPlot("bar", data,layout)
+
+
+
+
+})}
+
+getData()
